29087a3b18b535b99ede041ba1c20a2b4630a558
Jeremiah Tantongco <jtantongco@gmail.com>
2012-02-16 14:48:22 -0800

    some stuff to added to the default.php
    config updated:
    base_url -> added '/' to the base_url that was crashing the thing
    log_threshold -> changed from 0 to 4 allowing for error logging
    
    custom_helper updated:
    removed the public permission from functions to cause it was causing a crash
    
    default.php
    Added some stuff I need to think over

--- codeigniter/application/helpers/custom_helper.php
+++ codeigniter/application/helpers/custom_helper.php
@@ -6,12 +6,26 @@
 			function fun_name() ...
 		}
 	*/
+	/*
+	function init(){
+		$CI =& get_instance();
+		$CI->load->helper('url');
+		$CI->load->library('session');
+		$CI->load->model('users_model');
+		$CI->load->model('records_model');
+		$CI->load->model('sheets_model');
+		$CI->config->item('base_url');
+		return $CI;
+	}
+	*/
     function anchor_img($controller_method,$img){
+		//$CI =& get_instance();
+		//$CI->load->helper('url');
         return anchor($controller_method ,img(array('src'=> $img, 'border'=>'0', 'alt'=> 'welcome')));
     }   
 	
 	//Must be called at least once
-	public function render_popScript(){
+	function render_popScript(){
 		//SRC for js must be outside the codeigniter since you get 403 forbidden error if not outside (need public permission)
 		echo '	<script src="http://datapi.com/jtantongco/scripts/popUpMagnifier/PopBoxDocumentation_files/PopBox.js" type="text/javascript">
 				</script>
@@ -22,7 +36,7 @@
 				</script> '; 
 	}
 	
-	public function render_popImg($src,$caption,$id,$width,$height){
+	function render_popImg($src,$caption,$id,$width,$height){
 		echo 	' <img 	pbshowpopbar="true" 
 						pbcaption="'.$caption.'"
 						src="'.$src.'" 
--- default.php
+++ default.php
@@ -6,3 +6,19 @@
 Projects I have on the burner:</br>
 Build website </br>
 Build TD game </br>
+Today I learned section: </br>
+2/15/2012:
+Should always speak with the person before the message not the message before the person
+->Got my question answered from years back from asking to Piam
+
+Women not so adversarial, more concerned about personal safety and security... applications to everyone else.
+
+Application on how to network, My question was answered.
+Don't be so adversarial. Don't put 
+
+Don't kiss ass to shmooze up your position.
+KISS ASS TO ADD THAT PERSON TO YOUR PERSONAL NETWORK OF RESOURCES
+
+Re: Randy Pausch 
+My time of someone saying:
+What a good way to phrase you are being a jerk...
